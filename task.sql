27ed6b3730bd937207deac2c59353091573c6d8f
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-01-09 16:11:21 +0200

    the issues have been fixed

--- task.sql
+++ task.sql
@@ -1,17 +1,13 @@
--- Use our database
 USE ShopDB; 
 
--- Some data should be created outside the transaction (here)
 INSERT INTO Orders (CustomerID, Date)
-    VALUES (1, "2023-01-01");
+    VALUES (1, '2023-01-01');
 
-INSERT INTO OrderItems (OrderID, ProductID, Count)
-    VALUES (1, 1, 5)
-
--- Start the transaction 
 START TRANSACTION; 
 
--- And some data should be created inside the transaction 
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 5 WHERE ID = 1
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+    VALUES (LAST_INSERT_ID(), 1, 1);
 
-COMMIT; 
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
+
+ROLLBACK; 
